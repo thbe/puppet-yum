54b8176aafb38b98be30c2ecd8c7fa8e860de465
thbe <project@bendler-net.de>
2018-04-30 10:13:12 +0200

    Fix Travis CI build

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -17,7 +17,7 @@ RSpec.configure do |c|
     # Install module and dependencies
     puppet_module_install(:source => proj_root, :module_name => 'yum')
     hosts.each do |host|
-      [ 'puppetlabs-stdlib' ].each do |mod|
+      ['puppetlabs-stdlib'].each do |mod|
         on host, puppet('module', 'install', mod), { :acceptable_exit_codes => [0,1] }
       end
     end
